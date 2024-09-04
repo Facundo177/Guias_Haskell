de74eefabb540e7d80edf637f62507d562eb4584
Facundo177 <facundogaitansegura@gmail.com>
2024-09-04 17:33:09 -0300

    Avance de la segunda clase práctica, guia 3

--- guia_1.hs
+++ guia_1.hs
@@ -83,5 +83,126 @@ esMultiploDe num1 num2 = (mod num1 num2) == 0
 
 -- i)
 digitoUnidades :: Int -> Int
-digitoUnidades num = mod num 10
+digitoUnidades num = mod (absoluto num) 10
                                 
+-- j)
+digitoDecenas :: Int -> Int
+digitoDecenas num = div (mod (absoluto num) 100) 10
+
+
+
+-- Ej 3
+
+estanRelacionados :: Integer -> Integer -> Bool
+estanRelacionados num1 num2 = (mod num1 num2) == 0
+
+
+
+-- Ej 4
+
+type DuplaReales = (Float, Float)
+type TernaEnteros = (Int, Int, Int)
+
+-- a)
+prodInt :: DuplaReales -> DuplaReales -> Float
+prodInt (a0, a1) (b0, b1) = a0 * b0 + a1 * b1
+
+-- b)
+todoMenorFeo :: DuplaReales -> DuplaReales -> Bool
+todoMenorFeo a b = (fst a < fst b) && (snd a < snd b)
+
+todoMenor :: DuplaReales -> DuplaReales -> Bool
+todoMenor (a0, a1) (b0, b1) = (a0 < b0) && (a1 < b1) 
+
+-- c)
+distanciaPuntos :: DuplaReales -> DuplaReales -> Float
+distanciaPuntos (a0, a1) (b0, b1) = sqrt ( (b1 - a1)**2 + (b0 - a0)**2 )
+
+-- d)
+sumaTerna :: TernaEnteros -> Int
+sumaTerna (a, b, c) = a + b + c
+
+-- e)
+sumarSoloMultiplos :: TernaEnteros -> Int -> Int
+sumarSoloMultiplos (a, b ,c) d  | aEsMultiplo && bEsMultiplo && cEsMultiplo = a + b + c
+                                | aEsMultiplo && bEsMultiplo && not cEsMultiplo = a + b
+                                | aEsMultiplo && not bEsMultiplo && cEsMultiplo = a + c
+                                | not aEsMultiplo && bEsMultiplo && cEsMultiplo = b + c
+                                | aEsMultiplo = a
+                                | bEsMultiplo = b
+                                | cEsMultiplo = c
+                                | otherwise = 0
+                                where   aEsMultiplo = (mod a d) == 0
+                                        bEsMultiplo = (mod b d) == 0
+                                        cEsMultiplo = (mod c d) == 0
+
+
+sumarSoloMultiplosCorto :: TernaEnteros -> Int -> Int
+sumarSoloMultiplosCorto (a, b ,c) d  | aEsMultiplo && bEsMultiplo && cEsMultiplo = a + b + c
+                                | aEsMultiplo && bEsMultiplo = a + b
+                                | aEsMultiplo && cEsMultiplo = a + c
+                                | bEsMultiplo && cEsMultiplo = b + c
+                                | aEsMultiplo = a
+                                | bEsMultiplo = b
+                                | cEsMultiplo = c
+                                | otherwise = 0
+                                where   aEsMultiplo = (mod a d) == 0
+                                        bEsMultiplo = (mod b d) == 0
+                                        cEsMultiplo = (mod c d) == 0
+
+
+
+sumarSoloMultiplosAvanzado :: TernaEnteros -> Int -> Int
+sumarSoloMultiplosAvanzado (a, b ,c) d = (siEsMultiplo a d) + (siEsMultiplo b d) + (siEsMultiplo c d)
+                                        where siEsMultiplo x b  | (mod x b) == 0 = x
+                                                                | otherwise = 0
+
+
+-- f)
+posPrimerPar :: TernaEnteros -> Int
+posPrimerPar (a, b, c)  | (mod a 2) == 0 = 1
+                        | (mod b 2) == 0 = 2
+                        | (mod c 2) == 0 = 3
+                        | otherwise = 4
+                        
+-- g)
+crearPar :: a -> b -> (a, b)
+crearPar a b = (a, b)
+
+-- h)
+invertir :: (a, b) -> (b, a)
+invertir (a, b) = (b, a)
+
+-- i)
+-- Ya está hecho con DuplaReales
+
+
+
+-- Ej 5
+
+{- 
+fFunc :: Int -> Int
+fFunc n | n <= 7 = n*n 
+        | otherwise = 2*n - 1
+
+gFunc :: Int -> Int
+gFunc n | (mod n 2) == 0 = div n 2 
+        | otherwise = 3*n + 1
+-}
+
+todosMenores :: (Integer, Integer, Integer) -> Bool
+todosMenores (t0, t1, t2) = (f t0 > g t0) && (f t1 > g t1) && (f t2 > g t2)
+                    where   f n | n <= 7 = n*n 
+                                | otherwise = 2*n - 1
+                            g n | (mod n 2) == 0 = div n 2 
+                                | otherwise = 3*n + 1
+                        
+
+
+-- Ej 6
+type Anio = Integer
+type EsBisiesto = Bool
+
+bisiesto :: Anio -> EsBisiesto
+bisiesto anio   | not (mod anio 4 == 0) || ((mod anio 100 == 0) && not (mod anio 400 == 0)) = False
+                | otherwise = True
